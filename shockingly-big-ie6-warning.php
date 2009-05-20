bb5a4e7106963dcddac0e90245d819f54c10cac7
mschertel <mschertel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-20 17:54:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/shockingly-big-ie6-warning/tags/1.5.7@119263 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shockingly-big-ie6-warning.php
+++ shockingly-big-ie6-warning.php
@@ -27,82 +27,47 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 // FUNCTION: browser_detection - taken from here: http://techpatterns.com/downloads/php_browser_detection.php
 function browser_detection( $which_test ) {
-	// initialize variables
 	$browser_name = '';
 	$browser_number = '';
-	// get userAgent string
 	$browser_user_agent = ( isset( $_SERVER['HTTP_USER_AGENT'] ) ) ? strtolower( $_SERVER['HTTP_USER_AGENT'] ) : '';
-	//pack browser array
-	// values [0]= user agent identifier, lowercase, [1] = dom browser, [2] = shorthand for browser,
 	$a_browser_types[] = array('opera', true, 'op' );
 	$a_browser_types[] = array('msie', true, 'ie' );
 	$a_browser_types[] = array('konqueror', true, 'konq' );
 	$a_browser_types[] = array('safari', true, 'saf' );
 	$a_browser_types[] = array('gecko', true, 'moz' );
 	$a_browser_types[] = array('mozilla/4', false, 'ns4' );
-	# this will set a default 'unknown' value
 	$a_browser_types[] = array('other', false, 'other' );
-
 	$i_count = count($a_browser_types);
-	for ($i = 0; $i < $i_count; $i++)
-	{
+	for ($i = 0; $i < $i_count; $i++) {
 		$s_browser = $a_browser_types[$i][0];
 		$b_dom = $a_browser_types[$i][1];
 		$browser_name = $a_browser_types[$i][2];
-		// if the string identifier is found in the string
-		if (stristr($browser_user_agent, $s_browser)) 
-		{
-			// we are in this case actually searching for the 'rv' string, not the gecko string
-			// this test will fail on Galeon, since it has no rv number. You can change this to 
-			// searching for 'gecko' if you want, that will return the release date of the browser
-			if ( $browser_name == 'moz' )
-			{
+		if (stristr($browser_user_agent, $s_browser)) {
+			if ( $browser_name == 'moz' ) {
 				$s_browser = 'rv';
 			}
 			$browser_number = browser_version( $browser_user_agent, $s_browser );
 			break;
 		}
 	}
-	// which variable to return
-	if ( $which_test == 'browser' )
-	{
+	if ( $which_test == 'browser' ) {
 		return $browser_name;
 	}
-	elseif ( $which_test == 'number' )
-	{
-		# this will be null for default other category, so make sure to test for null
+	elseif ( $which_test == 'number' ) {
 		return $browser_number;
 	}
-
-	/* this returns both values, then you only have to call the function once, and get
-	 the information from the variable you have put it into when you called the function */
-	elseif ( $which_test == 'full' )
-	{
+	elseif ( $which_test == 'full' ) {
 		$a_browser_info = array( $browser_name, $browser_number );
 		return $a_browser_info;
 	}
 }
-
-// function returns browser number or gecko rv number
-// this function is called by above function, no need to mess with it unless you want to add more features
-function browser_version( $browser_user_agent, $search_string )
-{
-	$string_length = 8;// this is the maximum  length to search for a version number
-	//initialize browser number, will return '' if not found
+function browser_version( $browser_user_agent, $search_string ) {
+	$string_length = 8;
 	$browser_number = '';
-
-	// which parameter is calling it determines what is returned
 	$start_pos = strpos( $browser_user_agent, $search_string );
-	
-	// start the substring slice 1 space after the search string
 	$start_pos += strlen( $search_string ) + 1;
-	
-	// slice out the largest piece that is numeric, going down to zero, if zero, function returns ''.
-	for ( $i = $string_length; $i > 0 ; $i-- )
-	{
-		// is numeric makes sure that the whole substring is a number
-		if ( is_numeric( substr( $browser_user_agent, $start_pos, $i ) ) )
-		{
+	for ( $i = $string_length; $i > 0 ; $i-- ) {
+		if ( is_numeric( substr( $browser_user_agent, $start_pos, $i ) ) ) {
 			$browser_number = substr( $browser_user_agent, $start_pos, $i );
 			break;
 		}
@@ -245,8 +210,9 @@ function ie6w_head_center() {
 function ie6w_head_crash() {
 	$opt = get_option('ie6w_options');
 	//echo '<!-- ie6w CRASH ' . $opt['type'] . ' ' . $opt['test'] . ' -->';
-	echo '<!--[if lte IE 6]><style>*{position:relative}</style><table><input></table>
-	<STYLE>@;/*<![endif]-->';
+	if ( $a_browser_data[0] == 'ie' && $a_browser_data[1] <= 6 ) {
+		echo '<!--[if lte IE 6]><style>*{position:relative}</style><table><input></table><STYLE>@;/*<![endif]-->';
+	}
 }
 
 // INITIALIZATION - locales @ /lang/
